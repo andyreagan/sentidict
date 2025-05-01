152518e2aa6d3bde24e3f74c3d69ff52e4cc63be
Andy Reagan <andy@andyreagan.com>
2025-05-01 13:02:21 -0400

    Modernize package structure with pyproject.toml and update Python versions 3.9-3.12

--- setup.py
+++ setup.py
@@ -1,35 +1,50 @@
-# from distutils.core import setup
-from setuptools import setup
+from setuptools import setup, find_packages
+import os
 
-with open("README.markdown") as file:
-    long_description = file.read()
+# Define version in one place
+VERSION = "0.1.9"
 
-# get the files that are actually loaded by the program
-# to make this file, edit the default save flag in utils.openWithPath to be true
-# and then run functions.load_26()
-# (then, remove LIWC files so they don't get distributed)
-with open("datafiles.txt","r") as file:
+# Read the contents of README.markdown file
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.markdown'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# Get the data files
+with open(os.path.join(this_directory, "datafiles.txt"), "r") as file:
     data_files = [line.rstrip() for line in file]
     
 setup(
-    name = "sentidict",
-    packages = ["sentidict"],
-    package_data={"sentidict": ["static/*","templates/*"]+data_files},
-    version = "0.1.8",
-    description = "Utilities for dictionary-based sentiment analysis. Includes 28 sentiment dictionaries with loaders, scoring, and interactive visualization.",
-    long_description = long_description,
-    install_requires=["marisa_trie","numpy","jinja2"],
-    extras_require={
-        "dev": ["ipython","twine","Sphinx","recommonmark"],
-        # http://nose2.readthedocs.io/en/latest/plugins/coverage.html
-        "test": ["nose2","cov-core","scipy","jupyter"],
+    name="sentidict",
+    version=VERSION,
+    author="Andy Reagan",
+    author_email="andy@andyreagan.com",
+    description="Utilities for dictionary-based sentiment analysis. Includes 28 sentiment dictionaries with loaders, scoring, and interactive visualization.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/andyreagan/sentidict",
+    project_urls={
+        "Bug Tracker": "https://github.com/andyreagan/sentidict/issues",
     },
-    author = "Andy Reagan",
-    author_email = "andy@andyreagan.com",
-    url = "https://github.com/andyreagan/sentidict", 
-    download_url = "https://github.com/andyreagan/sentidict/tarball/0.1.8",
-    keywords = "sentiment emotion",
-    classifiers = [
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",],
-    )
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    package_data={"sentidict": ["static/*","templates/*"] + data_files},
+    python_requires=">=3.9",
+    install_requires=[
+        "marisa_trie",
+        "numpy",
+        "jinja2",
+    ],
+    extras_require={
+        "dev": ["ipython", "twine", "Sphinx", "recommonmark"],
+        "test": ["nose2", "cov-core", "scipy", "jupyter"],
+    },
+    keywords="sentiment emotion nlp text-analysis",
+)
